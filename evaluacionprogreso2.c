1898e8568f372d19439215afbe4683f06644a869
Pablo <pablo.munoz.proano@udla.edu.ec>
2023-12-06 15:50:23 -0500

    Abro la funcion main para trabajar por pasos el problema planteado

--- evaluacionprogreso2.c
+++ evaluacionprogreso2.c
@@ -5,9 +5,16 @@
 #define NUM_ALUMNOS 23
 #define NUM_PROGRESOS 3
 
-// Declarar funciones
+// Declarar funciones//
 void ingresarNotas(float calificaciones[NUM_ALUMNOS][NUM_PROGRESOS]);
 void calcularPromedioEstudiante(float calificaciones[NUM_ALUMNOS][NUM_PROGRESOS], float promedioEstudiantes[NUM_ALUMNOS]);
 void calcularPromedioAvance(float calificaciones[NUM_ALUMNOS][NUM_PROGRESOS], float promedioAvances[NUM_PROGRESOS]);
 int encontrarMejorEstudiante(float promedioEstudiantes[NUM_ALUMNOS]);
+
+//Abro la funcion main y realizo por pasos//
+int main() {
+    float calificaciones[NUM_ALUMNOS][NUM_PROGRESOS];
+    float promedioEstudiantes[NUM_ALUMNOS];
+    float promedioAvances[NUM_PROGRESOS];
+    int mejorEstudiante;
 
